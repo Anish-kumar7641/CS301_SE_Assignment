72de1f65eb114c1b28fe98fec4ea705fa587b23c
Anish-kumar7641 <72142986+Anish-kumar7641@users.noreply.github.com>
2023-03-26 23:04:31 +0530

    Software Engineering Assignment Added

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,35 @@
+{"login":{
+    "username": "Anish",
+    "password": "21bcs010"
+},"art":{
+    "music": {
+        "view": ["rock", "jazz", "blues"],
+        "buy": {
+            "rock": 10,
+            "jazz": 15,
+            "blues": 20
+        }
+    },
+    "dance": {
+        "view": ["ballet", "tap", "salsa"],
+        "buy": {
+            "ballet": 25,
+            "tap": 20,
+            "salsa": 30
+        }
+    },
+    "spoken word": {
+        "view": ["poetry", "storytelling"],
+        "buy": {
+            "poetry": 5,
+            "storytelling": 8
+        }
+    },
+    "regional theatricals": {
+        "view": ["nautanki", "jatra"],
+        "buy": {
+            "nautanki": 12,
+            "jatra": 18
+        }
+    }
+}}
